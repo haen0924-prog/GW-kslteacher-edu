3b1775582614ef3aa76b967061b35907bfac008d
haen0924-prog <haen0924@gwdeaf.or.kr>
2026-05-15 11:16:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,3 @@
-
 const SUPABASE_URL = 'https://njrbqfyxxwobthzrhqzm.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcmJxZnl4eHdvYnRoenJocXptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5NDkxODUsImV4cCI6MjA4OTUyNTE4NX0.8lw0F6jzaMHlBqaiHoEtywLsANdcQDLJVCqiSdwPFMg';
-
-
 const ADMIN_EMAILS = ['haen0924@gwdeaf.or.kr'];
